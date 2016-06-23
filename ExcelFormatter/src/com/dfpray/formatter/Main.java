c20fc5dd6fa7e3d154780a98982d958850ee8c59
Johnathan AVant <jaavant@wpi.edu>
2016-06-23 14:14:29 -0400

    Code to read from excel and process into a list of B.C was put into its own method.
    - Arraylist cards was intialized to an arraylist rather than null.

--- ExcelFormatter/src/com/dfpray/formatter/Main.java
+++ ExcelFormatter/src/com/dfpray/formatter/Main.java
@@ -1,6 +1,8 @@
 package com.dfpray.formatter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -12,9 +14,35 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
 public class Main {
-	@SuppressWarnings("resource")
+	
 	public static void main(String args[]){
-		ArrayList<BuissnessCard> cards;
+		
+		ArrayList<BuissnessCard> cards = new ArrayList<BuissnessCard>();
+	
+		//Get Cards from excel sheet
+		try {
+			cards = getCards("C:/Users/javant/Documents/Contacts");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Load into Gui
+		//allow the cards to be processed
+		//check if everything is added and everything
+		
+		
+		//Print to another file
+		
+	}
+	
+	/** Get's address of Excel document to read, then composes an array of B.C from reading file
+	 * 
+	 * @param address Path to file to read
+	 * @return	Array of B.C 
+	 * @throws IOException 
+	 */
+	private static ArrayList<BuissnessCard> getCards(String address) throws IOException{
 		File file;
 		FileInputStream fis;
 		XSSFWorkbook workbook;
@@ -22,12 +50,10 @@ public class Main {
 		Iterator<Row> rowIterator;
 		Row row;
 		Cell cell;
-		String cellString;
-
+		ArrayList<BuissnessCard> bCards = new ArrayList<BuissnessCard>();
 		
-		try{
-			//Get file * Should be replaced by directory explorer
-			file = new File("C:/Users/javant/Documents/Contacts");
+
+			file = new File(address);
 			
 			//Open up stream
 			fis = new FileInputStream(file);
@@ -35,34 +61,20 @@ public class Main {
 			//Create a Workbook instance
 			workbook = new XSSFWorkbook(fis);
 			
-			//Get first sheet form the workbook
+			//Get first sheet from the workbook
 			sheet = workbook.getSheetAt(0);
-			
-			//Iterate through each rows one by one
+
 			rowIterator = sheet.iterator();
 			
+			//Iterate through each rows one by one
 			while(rowIterator.hasNext()){
 				row = rowIterator.next();	
-			
-				//Get first and get String
 				cell = row.getCell(0);
-				
-				throw (new IncompleteException("Must create card"));
-	
-				
-			}
-			
-			
-			
-			
-		} catch (Exception e){
-			e.printStackTrace();
-			
-		}
+				bCards.add(new BuissnessCard(cell.getStringCellValue()));
+			}		
+			workbook.close();
 		
-		
-		
-		
+		return bCards;	
 	}
 }
 
